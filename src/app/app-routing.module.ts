ac2774cdac0d5acffea6aac829d8be0576b50c98
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-01-06 20:13:50 -0500

    repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { BoardUserComponent } from './our-services/board-user.component';
-import { BlogListComponent } from './portfolio/blog-list.component';
+import { BoardUserComponent } from './our-services/service-list.component';
+import { BlogListComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'services', component: BoardUserComponent },
   { path: 'portfolio', component: BlogListComponent },

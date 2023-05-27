80b4771aa68bb6d17470d137c161eb61ec4269da
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-05-27 11:20:11 -0400

    mobile margin fixes

--- src/app/portfolio-details/portfolio-details.component.ts
+++ src/app/portfolio-details/portfolio-details.component.ts
@@ -83,6 +83,12 @@ export class PortfolioDetailsComponent implements OnInit {
     }
   }
 
+  open(url: any, title: any, w: any, h: any){
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  }
+  
   rando() {
     return Math.floor(Math.random() * 100000);
   }
